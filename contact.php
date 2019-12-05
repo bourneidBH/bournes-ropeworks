1cfa8f3b8ddc70b1cdbedef4a6b9744a3c8c56f4
Jason Bourne <m.bourne@wi.rr.com>
2019-12-05 17:34:07 -0600

    fixed logo link

--- contact.php
+++ contact.php
@@ -37,8 +37,8 @@
         <div class="navbar-fixed">
             <nav>
                 <div class="nav-wrapper">
-                    <a href="#" class="brand-logo"><img src="assets/images/BournesRopeworks_logo-reverse.png" alt="Bourne's Ropeworks" height="80px"></a>
-                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <a href="index.html#home" class="brand-logo"><img src="assets/images/BournesRopeworks_logo-reverse.png" alt="Bourne's Ropeworks" height="80px"></a>
+                    <a href="index.html#home" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
                         <li><a href="index.html#services">Services</a></li>
                         <li><a href="index.html#marine">Marine</a></li>
